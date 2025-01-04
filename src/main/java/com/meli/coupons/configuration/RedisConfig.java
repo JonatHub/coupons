ea68f0afd5f17e44ba2c1ef1a5e5a679c4a3c951
Joao <0101lopezru@gmail.com>
2025-01-04 03:17:39 -0500

    Using docker compose and using network docker

--- src/main/java/com/meli/coupons/configuration/RedisConfig.java
+++ src/main/java/com/meli/coupons/configuration/RedisConfig.java
@@ -1,14 +1,5 @@
 package com.meli.coupons.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-package com.meli.coupons.configuration;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
